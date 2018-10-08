a072bbcbdc6e9e6d1f7c57a98cf896ce6324e738
msantamariaglobant <m.santamaria@globant.com>
2018-10-08 13:52:46 -0300

    delete extras

--- dockinit.sh
+++ dockinit.sh
@@ -6,9 +6,9 @@ mkdir -p projects
 
 cd projects/
 
-git clone -b develop --single-branch git@github.com:ImagineLearning/website-corporate.git corporate
-git clone -b develop --single-branch git@github.com:ImagineLearning/website-support.git support
-git clone -b develop --single-branch git@github.com:ImagineLearning/website-university.git university
+git clone -b develop --single-branch reponame corporate
+git clone -b develop --single-branch reponame support
+git clone -b develop --single-branch reponame university
 
 cd support/
 git submodule update --init --recursive
